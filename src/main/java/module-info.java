bb262867109c9b96cb13e75c88e3c617b4fc9341
Nikolay Petrov <sungon@gmail.com>
2025-10-12 10:29:26 -0400

    extract opvaultj

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module dev.ingon.opvaultfx {
     requires javafx.base;
     requires javafx.controls;
+    requires dev.ingon.opvault;
     requires dev.ingon.json.zero;
     
     requires transitive javafx.graphics;
     requires transitive java.sql;
     requires java.desktop;
     
-    exports dev.ingon.opvault;
     exports dev.ingon.opvaultfx;
 }
